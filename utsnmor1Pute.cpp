405e558db55c83b02543b05b7124667701be28dd
PutriFatmawati <49117972+PutriFatmawati@users.noreply.github.com>
2019-03-31 22:31:32 +0800

    Add files via upload

---
+++ utsnmor1Pute.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int data_mahasiswa;
+    cout <<"nama : Putri Fatmawati" <<endl;
+    cout<<"nim : 18291022" <<endl;
+    cout<<"kelas : tekomB" <<endl;
+    cout<<"tahun angkatan : 2018" <<endl;
+}
+
+
+
+
+
+
